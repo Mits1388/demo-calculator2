d1a3ed58fee155e7ea2f26c72bee415e7e6e0ba6
Mits16 <mits1388@gmail.com>
2024-10-17 21:13:37 +0300

    add minus multiply divide

--- src/main/java/by/itacademy/artsiommitskevich/calculator2/Calculator.java
+++ src/main/java/by/itacademy/artsiommitskevich/calculator2/Calculator.java
@@ -4,5 +4,14 @@ public class Calculator {
     public double sum(double a, double b) {
         return a + b;
     }
+    public double minus(double a, double b) {
+        return a - b;
+    }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    public double divide(double a, double b) {
+        return a / b;
+    }
 
 }
